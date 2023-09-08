70b1610f4820880e396850a55c70fc1508afae11
Mudoker <95454901+Mudoker@users.noreply.github.com>
2023-09-08 23:26:22 +0700

    Update Constants.swift

--- Desktop/Prismm/Prismm/Constants/Constants.swift
+++ Desktop/Prismm/Prismm/Constants/Constants.swift
@@ -13,4 +13,6 @@ struct Constants {
     static let lightThemeColor: Color = Color.pink
     static let buttonGradientColorDark: [Color] = [Color(red: 0.27, green: 1.00, blue: 0.79), Color(red: 0.59, green: 1.00, blue: 0.96), Color(red: 0.44, green: 0.57, blue: 0.96)]
     static let buttonGradientColorLight: [Color] = [Color(red: 0.96, green: 0.51, blue: 0.65), Color(red: 0.95, green: 0.00, blue: 0.53), Color(red: 0.44, green: 0.10, blue: 0.46)]
+    static let lightThemeAppLogo = "logolight"
+    static let DarkThemeAppLogo = "logodark"
 }
