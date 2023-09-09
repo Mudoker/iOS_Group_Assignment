f4954ca27370be3b94ae42fbf544f2afa292e2b6
Quoc Doan Huu <s3927776@iMac-92.RMIT.EDU.VN>
2023-09-09 12:02:13 +0700

    ok

--- Prismm/Prismm/Components/Models/Message.swift
+++
@@ -1,8 +0,0 @@
-//
-//  Message.swift
-//  Prismm
-//
-//  Created by Quoc Doan Huu on 09/09/2023.
-//
-
-import Foundation
--- Prismm/Prismm/Components/ViewModels/LoginViewModel.swift
+++
@@ -1,8 +0,0 @@
-//
-//  LoginViewModel.swift
-//  Prismm
-//
-//  Created by Quoc Doan Huu on 09/09/2023.
-//
-
-import Foundation
