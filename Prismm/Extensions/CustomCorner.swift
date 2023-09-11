bf4ea9eaf4b2a4039af90cf70d8a5e9b3964767d
Tran Vu Quang Anh <67688541+tranvuquanganh87@users.noreply.github.com>
2023-09-11 13:58:08 +0700

    fix UI splashScreen

--- Prismm/Extensions/CustomCorner.swift
+++ Prismm/Extensions/CustomCorner.swift
@@ -2,17 +2,7 @@
 //  CustomCorner.swift
 //  Prismm
 //
-//  Created by Tran Vu Quang Anh  on 10/09/2023.
+//  Created by Tran Vu Quang Anh  on 11/09/2023.
 //
-import Foundation
-import SwiftUI
 
-struct CustomCorners : Shape{
-    var corners : UIRectCorner
-    var radius : CGFloat
-    
-    func path(in rect : CGRect) -> Path {
-        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-        return Path(path.cgPath)
-    }
-}
+import Foundation
