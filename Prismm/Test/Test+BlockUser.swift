88d92cf26c0393d914d6549037535a0dead8e2af
BaoNgoc <s3924436@rmit.edu.vn>
2023-09-23 15:42:36 +0700

    oke luon

--- Prismm/Test/Test+BlockUser.swift
+++ Prismm/Test/Test+BlockUser.swift
@@ -3,38 +3,3 @@
 //  Prismm
 //
 //  Created by Nguyen Dinh Viet on 21/09/2023.
-//
-
-import SwiftUI
-
-struct Test_BlockUser: View {
-    @ObservedObject var homeVM = HomeViewModel()
-    var body: some View {
-        
-        VStack {
-            Button(action: {
-                Task {
-                    try await homeVM.blockOtherUser(forUserID: "OXvEytbtEYOZTTbksTJcp4US0C02")
-                }
-            }) {
-                Text("Block user")
-            }
-            .padding(.vertical)
-            
-            Button(action: {
-                Task {
-                    try await homeVM.unblockOtherUser(forUserID: "OXvEytbtEYOZTTbksTJcp4US0C02")
-                }
-            }) {
-                Text("Unblock user")
-            }
-        }
-        
-    }
-}
-
-struct Test_BlockUser_Previews: PreviewProvider {
-    static var previews: some View {
-        Test_BlockUser()
-    }
-}
